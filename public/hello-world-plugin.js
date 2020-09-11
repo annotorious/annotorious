6d0d64e2f82cd42a8fffbf7e22bf3cd0cf4f363a
Rainer Simon <rainer.simon@ait.ac.at>
2020-09-11 10:02:02 +0200

    Added minimal plugin example

--- public/hello-world-plugin.js
+++ public/hello-world-plugin.js
@@ -6,16 +6,35 @@
  */
 var HelloWorldPlugin = function(args) {
 
-  this.annotation = args.annotation;
-  this.readOnly = args.readOnly;
+  var addTag = function(evt) {
+    
+    const tag = evt.target.dataset.tag;
 
-  this.onAppendBody = args.onAppendBody;
-  this.onUpdateBody = args.onUpdateBody;
-  this.onDeleteBody = args.onDeleteBody;
+    args.onAppendBody({
+      type: 'TextualBody',
+      purpose: 'highlighting',
+      value: tag
+    });
+  }
 
-  var div = document.createElement('div');
-  div.className = 'helloworld-plugin';
-  div.innerHTML = 'Hello World';
+  var createButton = function(value) {
+    var button = document.createElement('button');
+    button.dataset.tag = value;
+    button.innerHTML = value;
+    button.addEventListener('click', addTag); 
+    return button;
+  }
 
-  return div;
+  var container = document.createElement('div');
+  container.className = 'helloworld-plugin';
+  
+  var button1 = createButton('RED');
+  var button2 = createButton('GREEN');
+  var button3 = createButton('BLUE');
+
+  container.appendChild(button1);
+  container.appendChild(button2);
+  container.appendChild(button3);
+
+  return container;
 }
