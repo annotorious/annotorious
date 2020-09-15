de013f769e8ba59bd90256fa9dbe688a03e07e0e
Rainer Simon <rainer.simon@ait.ac.at>
2020-09-15 07:04:39 +0200

    Bugfix

--- src/SVG.js
+++ src/SVG.js
@@ -4,6 +4,7 @@ export const SVG_NAMESPACE = 'http://www.w3.org/2000/svg';
 // via .setAttribute
 export const addClass = (el, className) => {
   const classNames = new Set(el.getAttribute('class').split(' '));
+  classNames.add(className);
   el.setAttribute('class', Array.from(classNames).join(' '));
 }
 
