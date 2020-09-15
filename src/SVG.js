8c6292b6e85551681436eeaa6cc44412d4564feb
Rainer Simon <rainer.simon@ait.ac.at>
2020-09-15 07:03:22 +0200

    Fixes #55

--- src/SVG.js
+++ src/SVG.js
@@ -4,8 +4,7 @@ export const SVG_NAMESPACE = 'http://www.w3.org/2000/svg';
 // via .setAttribute
 export const addClass = (el, className) => {
   const classNames = new Set(el.getAttribute('class').split(' '));
-  classNames.add(className)
-  el.setAttribute('class', classNames.join(' '));
+  el.setAttribute('class', Array.from(classNames).join(' '));
 }
 
 export const removeClass = (el, className) => {
