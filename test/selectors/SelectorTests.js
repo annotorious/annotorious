273e566d96cac66482e68f450cd72eb4cfbf881f
Rainer Simon <aboutgeo@gmail.com>
2021-07-10 10:13:14 +0200

    Unit test

--- test/selectors/SelectorTests.js
+++ test/selectors/SelectorTests.js
@@ -1,16 +1,26 @@
 import assert from 'assert';
+import WebAnnotation from '@recogito/recogito-client-core/src/WebAnnotation';
 import { svgArea } from '../../src/selectors/EmbeddedSVG';
 
-const POINTS = [
-  [172,210],
-  [456,169],
-  [440.6346478496909,62.56682900761504],
-  [156.6346478496909,103.56682900761504]
-];
+const fixtureAnnotation = new WebAnnotation({ 
+  "@context": "http://www.w3.org/ns/anno.jsonld",
+  "id": "#218d01ff-f077-4cc3-992d-1c81c426e51b",
+  "type": "Annotation",
+  "body": [{
+    "type": "TextualBody",
+    "value": "Evangelische Pfarrkirche Hallstatt"
+  }],
+  "target": {
+    "selector": [{
+      "type": "SvgSelector",
+      "value": "<svg><polygon points='172,160 199,15 223,6 239,132 244,173 285,179 313,208 313,251 218,306 170,290 172,160'></polygon></svg>"
+    }]
+  }
+});
 
-describe('polygonArea', function() {
-  it('should return a non-zero value for the test point array', () => {
-    const area = svgArea(POINTS);
+describe('svgArea', function() {
+  it('should return a non-zero value for polygon annotations', () => {
+    const area = svgArea(fixtureAnnotation);
     assert(area > 0);
   });
 });
