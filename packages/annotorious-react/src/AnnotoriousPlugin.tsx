65e3807b054b80db733822b3c857597d44d10231
Rainer Simon <rainer@rainersimon.io>
2023-10-12 14:16:07 +0200

    Added generic React plugin utility

---
+++ packages/annotorious-react/src/AnnotoriousPlugin.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { Annotation, Annotator } from '@annotorious/annotorious';
+import { useAnnotator } from './Annotorious';
+
+export interface AnnotoriousPluginProps <I extends Annotation, E extends unknown> {
+
+  plugin: (anno: Annotator<I, E>, opts?: Object) => { unmount?: () => void };
+
+  opts?: Object;
+
+}
+
+export const AnnotoriousPlugin = <I extends Annotation = Annotation, E extends unknown = unknown>(props: AnnotoriousPluginProps<I, E>) => {
+  const { plugin, opts } = props;
+
+  const anno = useAnnotator<Annotator<I, E>>();
+
+  useEffect(() => {
+    if (anno) {
+      const p = plugin(anno, opts);
+
+      return () => {
+        if (p.unmount)
+          p.unmount();
+      }
+    }
+  }, [anno]);
+
+}
