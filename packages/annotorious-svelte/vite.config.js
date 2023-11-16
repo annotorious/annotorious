3918f7fd14949ba8661cc682ff7691552a2e30ad
Rainer Simon <rainer@rainersimon.io>
2023-11-16 13:56:27 +0100

    Svelte build tweak

--- packages/annotorious-svelte/vite.config.js
+++ packages/annotorious-svelte/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
+import dts from 'vite-plugin-dts';
 
 import * as packageJson from './package.json';
 
 export default defineConfig({
   plugins: [
-    svelte({ preprocess: sveltePreprocess() })
+    svelte({ preprocess: sveltePreprocess() }),
+    dts({
+      insertTypesEntry: true,
+      include: ['./src/'],
+      entryRoot: './src'
+    })
   ],
   server: {
     open: '/test/index.html'
