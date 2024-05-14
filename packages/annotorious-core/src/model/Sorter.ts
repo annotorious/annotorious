cc2c13512486b0b098f7ab57f36554c7844fac95
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2024-05-14 18:12:31 +0300

    Added default annotation sorter

--- packages/annotorious-core/src/model/Sorter.ts
+++ packages/annotorious-core/src/model/Sorter.ts
@@ -1,3 +1,10 @@
 import type { Annotation } from './Annotation';
 
 export type Sorter<T extends Annotation = Annotation> = (a: T, b: T) => number;
+
+export const DEFAULT_ANNOTATION_SORTER: Sorter = (a, b) => {
+  const { target: { created: createdA } } = a;
+  const { target: { created: createdB } } = b;
+
+  return (createdA?.getTime() || 0) - (createdB?.getTime() || 0);
+};
