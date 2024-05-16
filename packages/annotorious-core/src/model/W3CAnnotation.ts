b93f60e9d123ba8e9c2e9fe162d8ba47d1dd7d07
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2024-05-16 14:01:46 +0300

    Added `modified` field handling for the `W3CAnnotationBody`

--- packages/annotorious-core/src/model/W3CAnnotation.ts
+++ packages/annotorious-core/src/model/W3CAnnotation.ts
@@ -8,9 +8,9 @@ export interface W3CAnnotation {
 
   id: string;
 
-  created?: string;
-
   creator?: W3CUser;
+
+  created?: string;
 
   modified?: string;
 
@@ -44,9 +44,11 @@ export interface W3CAnnotationBody {
 
   source?: string;
 
+  creator?: W3CUser;
+
   created?: string;
 
-  creator?: W3CUser;
+  modified?: string;
 
 }
 
@@ -89,7 +91,7 @@ export const parseW3CBodies = (
 ) : AnnotationBody[] => (Array.isArray(body) ? body : [body]).map(body => {
 
   // Exctract properties that conform to the internal model, but keep custom props
-  const { id, type, purpose, value, created, creator, ...rest } = body;
+  const { id, type, purpose, value, created, modified, creator, ...rest } = body;
 
   // The internal model strictly requires IDs. (Because multi-user scenarios
   // will have problems without them.) In the W3C model, bodys *may* have IDs.
@@ -102,8 +104,9 @@ export const parseW3CBodies = (
     type,
     purpose,
     value,
-    created: created ? new Date(created) : undefined,
     creator: parseW3CUser(creator),
+    created: created ? new Date(created) : undefined,
+    updated: modified ? new Date(modified) : undefined,
     ...rest
   }
 
@@ -118,5 +121,9 @@ export const serializeW3CBodies = (bodies: AnnotationBody[]): W3CAnnotationBody[
     if (w3c.id?.startsWith('temp-'))
       delete w3c.id;
 
-    return { ...w3c, created: w3c.created?.toISOString() };
+    return {
+      ...w3c,
+      created: w3c.created?.toISOString(),
+      modified: w3c.updated?.toISOString()
+    };
   });
