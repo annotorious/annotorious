d7be2e6ad5393d5a7f82b50bd8ae90ee860c6715
Adrien Fischer <po8rewq@users.noreply.github.com>
2024-08-01 11:14:53 +0200

    applying style based on props

--- packages/annotorious/src/annotation/utils/styling.ts
+++ packages/annotorious/src/annotation/utils/styling.ts
@@ -1,22 +1,28 @@
-import type { DrawingStyleExpression } from '@annotorious/core';
-import type { ImageAnnotation } from '../../model';
+import type { DrawingStyleExpression } from "@annotorious/core";
+import type { ImageAnnotation } from "../../model";
 
 export const computeStyle = (
-  annotation: ImageAnnotation, 
+  annotation: ImageAnnotation,
   style?: DrawingStyleExpression<ImageAnnotation>
 ) => {
-  const computed = typeof style === 'function' ? style(annotation) : style;
+  const computed = typeof style === "function" ? style(annotation) : style;
 
   if (computed) {
-    const { fill, fillOpacity } = computed;
+    const { fill, fillOpacity, stroke, strokeWidth, strokeOpacity } = computed;
 
-    let css = '';
-    
-    if (fill)
-      css += `fill:${fill};stroke:${fill};`;
-    
-    css += `fill-opacity:${fillOpacity || '0.25'};`;
+    let css = "";
+
+    if (fill) {
+      css += `fill:${fill};`;
+      css += `fill-opacity:${fillOpacity || "0.25"};`;
+    }
+
+    if (stroke) {
+      css += `stroke:${stroke};`;
+      css += `stroke-width:${strokeWidth || "1"};`;
+      css += `stroke-opacity:${strokeOpacity || "1"};`;
+    }
 
     return css;
   }
-}
+};
