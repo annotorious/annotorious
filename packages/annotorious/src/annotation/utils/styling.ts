4de24c45c25a5995410d5b93b99cf62bec5a3b25
Adrien Fischer <po8rewq@users.noreply.github.com>
2024-08-01 11:23:02 +0200

    reverting quotes

--- packages/annotorious/src/annotation/utils/styling.ts
+++ packages/annotorious/src/annotation/utils/styling.ts
@@ -1,26 +1,26 @@
-import type { DrawingStyleExpression } from "@annotorious/core";
-import type { ImageAnnotation } from "../../model";
+import type { DrawingStyleExpression } from '@annotorious/core';
+import type { ImageAnnotation } from '../../model';
 
 export const computeStyle = (
   annotation: ImageAnnotation,
   style?: DrawingStyleExpression<ImageAnnotation>
 ) => {
-  const computed = typeof style === "function" ? style(annotation) : style;
+  const computed = typeof style === 'function' ? style(annotation) : style;
 
   if (computed) {
     const { fill, fillOpacity, stroke, strokeWidth, strokeOpacity } = computed;
 
-    let css = "";
+    let css = '';
 
     if (fill) {
       css += `fill:${fill};`;
-      css += `fill-opacity:${fillOpacity || "0.25"};`;
+      css += `fill-opacity:${fillOpacity || '0.25'};`;
     }
 
     if (stroke) {
       css += `stroke:${stroke};`;
-      css += `stroke-width:${strokeWidth || "1"};`;
-      css += `stroke-opacity:${strokeOpacity || "1"};`;
+      css += `stroke-width:${strokeWidth || '1'};`;
+      css += `stroke-opacity:${strokeOpacity || '1'};`;
     }
 
     return css;
