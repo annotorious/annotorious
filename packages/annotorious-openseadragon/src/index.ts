b19fb0097845ebc4bce1b38b10c45daad1bd9b40
Rainer Simon <rainer@rainersimon.io>
2024-09-04 09:58:06 +0200

    Cleaned up re-exports

--- packages/annotorious-openseadragon/src/index.ts
+++ packages/annotorious-openseadragon/src/index.ts
@@ -25,17 +25,11 @@ export type {
   W3CAnnotationTarget
 } from '@annotorious/core';
 
-import { 
-  createBody as _createBody,
-  defaultColorProvider as _defaultColorProvider,
-  UserSelectAction as _UserSelectAction
+export { 
+  createBody,
+  defaultColorProvider,
+  UserSelectAction
 } from '@annotorious/core'; 
-
-export {
-  _createBody as createBody,
-  _defaultColorProvider as defaultColorProvider,
-  _UserSelectAction as UserSelectAction
-}
 
 // Essential re-exports from @annotorious/annotorious 
 export type {
@@ -59,12 +53,8 @@ export type {
   W3CImageFormatAdapterOpts
 } from '@annotorious/annotorious';
 
-import {   
-  createImageAnnotator as _createImageAnnotator,
-  ShapeType as _ShapeType,
-  W3CImageFormat as _W3CImageFormat
+export {   
+  createImageAnnotator,
+  ShapeType,
+  W3CImageFormat
 } from '@annotorious/annotorious';
-
-export const createImageAnnotator = _createImageAnnotator;
-export const ShapeType = _ShapeType;
-export const W3CImageFormat = _W3CImageFormat;
--- packages/annotorious-react/src/index.ts
+++ packages/annotorious-react/src/index.ts
@@ -40,19 +40,13 @@ export type {
   W3CAnnotationTarget
 } from '@annotorious/core';
 
-import {
-  createAnonymousGuest as _createAnonymousGuest, 
-  createBody as _createBody,
-  defaultColorProvider as _defaultColorProvider,
-  Origin as _Origin,
-  UserSelectAction as _UserSelectAction
+export {
+  createAnonymousGuest, 
+  createBody,
+  defaultColorProvider,
+  Origin,
+  UserSelectAction
 } from '@annotorious/core';
-
-export { _createAnonymousGuest as createAnonymousGuest };
-export { _createBody as createBody };
-export { _defaultColorProvider as defaultColorProvider };
-export { _Origin as Origin };
-export { _UserSelectAction as UserSelectAction };
 
 // Essential re-exports from @annotorious/annotorious 
 export type {
@@ -75,15 +69,11 @@ export type {
   W3CImageFormatAdapterOpts
 } from '@annotorious/annotorious';
 
-import {   
-  createImageAnnotator as _createImageAnnotator,
-  ShapeType as _ShapeType,
-  W3CImageFormat as _W3CImageFormat
+export {   
+  createImageAnnotator,
+  ShapeType,
+  W3CImageFormat
 } from '@annotorious/annotorious';
-
-export const createImageAnnotator = _createImageAnnotator;
-export const ShapeType = _ShapeType;
-export const W3CImageFormat = _W3CImageFormat;
 
 // Essential re-exports from @annotorious/openseadragon 
 export type {
