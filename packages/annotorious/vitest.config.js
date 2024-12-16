597b7c4ecd4d35f00f03d7089f2a1b61669a1c8b
Frederic Lamsens <frederic.lamsens@ugent.be>
2024-12-16 12:09:12 +0100

    chore: add missing svelte plugin to vitest config

--- packages/annotorious/vitest.config.js
+++ packages/annotorious/vitest.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vitest/config';
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
+  plugins: [
+      svelte()
+  ],
   test: {
     environment: 'jsdom',
     watch: false
