26444dae14f311369ec358e9466287743d168b48
Rainer Simon <rainer@rainersimon.io>
2025-01-24 14:34:50 +0100

    v3.1.0

--- packages/annotorious-react/src/useDebounce.ts
+++ packages/annotorious-react/src/useDebounce.ts
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from 'react';
 export const useDebounce = (value: any, delay: number) => {
   const [debouncedValue, setDebouncedValue] = useState<any>(value);
 
-  const timerRef = useRef<ReturnType<typeof setTimeout>>();
+  const timerRef = useRef<ReturnType<typeof setTimeout>>(undefined);
 
   useEffect(() => {
     timerRef.current = 
