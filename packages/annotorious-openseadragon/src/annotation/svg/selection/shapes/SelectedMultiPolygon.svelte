c5dd122d180153d974222e1446fb3fd42a896ebe
Rainer Simon <rainer@rainersimon.io>
2025-05-26 14:17:44 +0200

    Bugfix: rendering multipolygon in multi-selection

---
+++ packages/annotorious-openseadragon/src/annotation/svg/selection/shapes/SelectedMultiPolygon.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { multipolygonElementToPath } from '@annotorious/annotorious';
+  import type { ImageAnnotation, MultiPolygonGeometry } from '@annotorious/annotorious';
+  
+  /** Props **/
+  export let annotation: ImageAnnotation;
+
+  const { polygons } = annotation.target.selector.geometry as MultiPolygonGeometry;
+</script>
+
+<g>
+  {#each polygons as polygonElement}
+    <path 
+      fill-rule="evenodd"
+      d={multipolygonElementToPath(polygonElement)} />
+
+    <path 
+      class="a9s-inner"
+      fill-rule="evenodd"
+      d={multipolygonElementToPath(polygonElement)} />
+  {/each}
+</g>
