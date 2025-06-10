b982f4184310917a9fda03b40e477efba58dbf11
Rainer Simon <rainer@rainersimon.io>
2025-06-10 14:40:45 +0200

    Bugfix

--- packages/annotorious-react/src/openseadragon/OpenSeadragonHoverTooltip.tsx
+++ packages/annotorious-react/src/openseadragon/OpenSeadragonHoverTooltip.tsx
@@ -54,7 +54,7 @@ export const OpenSeadragonHoverTooltip = (props: OpenSeadragonHoverTooltipProps)
     const unsubscribe = anno.state.hover.subscribe(onHover);
 
     return () => {
-      viewer.element.removeEventListener('pointermove', onPointerMove);
+      viewer.element?.removeEventListener('pointermove', onPointerMove);
       unsubscribe();
     }
   }, [anno, viewer]);
