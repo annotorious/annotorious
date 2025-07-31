b041ffba1a33e188522d2cc3d0981ef1433c0def
Rainer Simon <rainer@rainersimon.io>
2025-07-31 09:16:10 +0200

    Export tweak

--- packages/annotorious/src/annotation/editors/index.ts
+++ packages/annotorious/src/annotation/editors/index.ts
@@ -5,3 +5,4 @@ export * from './editorsRegistry';
 export { default as Editor } from './Editor.svelte';
 export { default as EditorMount } from './EditorMount.svelte';
 export { default as Handle } from './Handle.svelte';
+export { default as MidpointHandle } from './MidpointHandle.svelte';
